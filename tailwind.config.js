37bf6e18812e083c4eff290f37940b627a7017c6
Mohanna Nabhan <mohanna.nabhant@gmail.com>
2024-02-24 17:29:58 +0100

    p

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import animations from "@midudev/tailwind-animations";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [animations],
+};
